cbe983c7a269d0508e28780791541be101faab16
Liam <jackirelandui@gmail.com>
2021-05-29 20:07:12 -0400

    Make Lombok global dependency

--- legacy/src/main/java/com/birthdates/videotominecraft/legacy/Legacy.java
+++ legacy/src/main/java/com/birthdates/videotominecraft/legacy/Legacy.java
@@ -1,21 +1,23 @@
 package com.birthdates.videotominecraft.legacy;
 
+import lombok.experimental.UtilityClass;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.map.MapView;
 
+@UtilityClass
 public class Legacy {
 
-    public static ItemStack getMapItem(MapView mapView) {
+    public ItemStack getMapItem(MapView mapView) {
         return new ItemStack(Material.MAP, 1, mapView.getId());
     }
 
-    public static boolean isMainHandAir(Player player) {
+    public boolean isMainHandAir(Player player) {
         return player.getItemInHand() != null && player.getItemInHand().getType() == Material.AIR;
     }
 
-    public static void setItemInHand(Player player, ItemStack itemStack) {
+    public void setItemInHand(Player player, ItemStack itemStack) {
         player.setItemInHand(itemStack);
     }
 
