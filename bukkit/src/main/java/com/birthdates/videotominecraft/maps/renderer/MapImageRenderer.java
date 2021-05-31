3add08bed8bcae211ad5007cecbc1b5b1aa16b93
Liam <jackirelandui@gmail.com>
2021-05-31 09:35:54 -0400

    Set buffer field instead of using setPixel
    This can save a lot of performance (almost double) when it comes to the end of rendering a movie/video. This occurs because MapCanvas#setPixel is essentially redoing calculations we've already done. So to combat that, I've used some reflection to cut down the calculations in half.

--- bukkit/src/main/java/com/birthdates/videotominecraft/maps/renderer/MapImageRenderer.java
+++ bukkit/src/main/java/com/birthdates/videotominecraft/maps/renderer/MapImageRenderer.java
@@ -6,6 +6,8 @@ import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
 
+import java.lang.reflect.Field;
+
 /**
  * Class used to render images quickly to a map canvas
  */
@@ -14,6 +16,7 @@ public class MapImageRenderer extends MapRenderer {
     private MapView mapView;
     private MapCanvas mapCanvas;
     private boolean imageDrawn;
+    private Field bufferField;
 
     /**
      * Draw raw pixels to the canvas
@@ -21,13 +24,44 @@ public class MapImageRenderer extends MapRenderer {
      * @param pixels {@link Maps#getResolution()}x{@link Maps#getResolution()} pixel array
      */
     public void drawRawPixels(byte[] pixels) {
+        if(trySetBuffer(pixels)) {
+            return;
+        }
         int mapRes = Maps.getResolution();
+        int bit = Maps.getBit();
+
         for (int x = 0; x < mapRes; ++x) {
             for (int y = 0; y < mapRes; ++y) {
-                mapCanvas.setPixel(x, y, pixels[y * mapRes + x]);
+                mapCanvas.setPixel(x, y, pixels[(y << bit) + x]);
             }
         }
         imageDrawn = true;
+    }
+
+    /**
+     * Instead of using {@link MapCanvas#setPixel(int, int, byte)} which is a repeat of what calculations we've already done,
+     * We can use reflection to set the private buffer field that {@link MapCanvas#setPixel(int, int, byte)} changes.
+     * @param pixels New pixels
+     * @return If we changed the field
+     */
+    private boolean trySetBuffer(byte[] pixels) {
+        if(bufferField == null)
+            return false;
+        try {
+            bufferField.set(mapCanvas, pixels);
+        } catch(IllegalAccessException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+        imageDrawn = true;
+        return true;
+    }
+
+    private void getBufferField() {
+        try {
+            bufferField = mapCanvas.getClass().getDeclaredField("buffer");
+            bufferField.setAccessible(true);
+        } catch(NoSuchFieldException ignored) {}
     }
 
     public void sendToPlayer(Player player) {
@@ -38,7 +72,10 @@ public class MapImageRenderer extends MapRenderer {
 
     @Override
     public void render(MapView mapView, MapCanvas mapCanvas, Player player) {
+        if(this.mapCanvas != null && this.mapView != null)
+            return;
         this.mapCanvas = mapCanvas;
         this.mapView = mapView;
+        getBufferField();
     }
 }
