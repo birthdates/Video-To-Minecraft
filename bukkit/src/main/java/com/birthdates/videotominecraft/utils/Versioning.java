3da5fe88f8f5bbb8083b78d8f4e044e099451fb7
Liam <jackirelandui@gmail.com>
2021-06-01 17:58:40 -0400

    Remove Version class and just use int

--- bukkit/src/main/java/com/birthdates/videotominecraft/utils/Versioning.java
+++ bukkit/src/main/java/com/birthdates/videotominecraft/utils/Versioning.java
@@ -1,16 +1,15 @@
 package com.birthdates.videotominecraft.utils;
 
-import lombok.AllArgsConstructor;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 
 @UtilityClass
 public class Versioning {
 
-    private final Version bukkitVersion = parseVersion(Bukkit.getBukkitVersion());
+    private final int bukkitVersion = parseVersion(Bukkit.getBukkitVersion());
 
     public boolean isAheadOrEqualTo(int minor) {
-        return bukkitVersion.minor >= minor;
+        return bukkitVersion >= minor;
     }
 
     public boolean isBehind(int minor) {
@@ -26,13 +25,8 @@ public class Versioning {
         return isBehind(13);
     }
 
-    private Version parseVersion(String ver) {
+    private int parseVersion(String ver) {
         String[] numbers = ver.split("\\.");
-        return new Version(Integer.parseInt(numbers[1]));
-    }
-
-    @AllArgsConstructor
-    private static class Version {
-        private final int minor; //major & patch is useless in our case
+        return Integer.parseInt(numbers[1]);
     }
 }
