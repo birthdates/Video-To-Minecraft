cbf58cc87b87bca9abc4dca72d32aaf5b2ce3b8a
Liam <jackirelandui@gmail.com>
2021-06-14 09:10:56 -0400

    Add support for snapshots

--- bukkit/src/main/java/com/birthdates/videotominecraft/utils/Versioning.java
+++ bukkit/src/main/java/com/birthdates/videotominecraft/utils/Versioning.java
@@ -3,9 +3,13 @@ package com.birthdates.videotominecraft.utils;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @UtilityClass
 public class Versioning {
 
+    private final Pattern versionPattern = Pattern.compile("[0-9]\\.[0-9]([0-9]|)"); //matches major.miner (i.e 1.17)
     private final int bukkitVersion = parseVersion(Bukkit.getBukkitVersion());
 
     public boolean isAheadOrEqualTo(int minor) {
@@ -26,6 +30,9 @@ public class Versioning {
     }
 
     private int parseVersion(String ver) {
+        Matcher matcher = versionPattern.matcher(ver);
+        if(matcher.find())
+            ver = matcher.group();
         String[] numbers = ver.split("\\.");
         return Integer.parseInt(numbers[1]);
     }
